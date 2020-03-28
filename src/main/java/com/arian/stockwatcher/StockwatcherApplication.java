0d4f2869e06370558514e8e022ae165503993895
Arian Hosseinzadeh <aryan.hosseinzadeh@gmail.com>
2020-03-27 23:25:39 -0700

    initialize the spring boot app

---
+++ src/main/java/com/arian/stockwatcher/StockwatcherApplication.java
@@ -0,0 +1,15 @@
+package com.arian.stockwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+public class StockwatcherApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StockwatcherApplication.class, args);
+	}
+
+}
