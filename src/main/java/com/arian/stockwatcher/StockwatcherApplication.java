0682890f45021984e8f0bc2fafb8bad7ab6858c5
Arian Hosseinzadeh <aryan.hosseinzadeh@gmail.com>
2020-03-28 09:58:32 -0700

    Spring boot app to stay up

--- src/main/java/com/arian/stockwatcher/StockwatcherApplication.java
+++ src/main/java/com/arian/stockwatcher/StockwatcherApplication.java
@@ -3,13 +3,22 @@ package com.arian.stockwatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@ImportResource("classpath*:application-context.xml")
 public class StockwatcherApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(StockwatcherApplication.class, args);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public void logGet() {
+		System.out.println("Hello!");
 	}
 
 }
